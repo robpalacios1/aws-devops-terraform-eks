c08c6c43e36646fe9b01f42043dde354ad7901bc
roberto <robpalacios1@hotmail.com>
2026-08-27 12:11:14 -0500

    feat: configure EKS module and define cluster output attributes for development environment

---
+++ terraform/modules/eks/outputs.tf
@@ -0,0 +1,24 @@
+output "cluster_id" {
+  description = "EKS cluster ID"
+  value       = aws_eks_cluster.main.id
+}
+
+output "cluster_endpoint" {
+  description = "EKS cluster name"
+  value       = aws_eks_cluster.main.endpoint
+}
+
+output "cluster_certificate_authority_data" {
+  description = "EKS cluster certificate authority data"
+  value       = aws_eks_cluster.main.certificate_authority[0].data
+}
+
+output "node_group_id" {
+  description = "EKS node group ID"
+  value       = aws_eks_node_group.main.id
+}
+
+output "cluster_oidc_issuer_url"{
+  description = "URL from emisor OIDC of EKS cluster"
+  value       = aws_eks_cluster.main.identity[0].oidc[0].issuer
+}
