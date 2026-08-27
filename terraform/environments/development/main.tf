562f2bead2256211344861756e8e92823fe22ad0
roberto <robpalacios1@hotmail.com>
2026-08-27 12:11:33 -0500

    feat: add VPC and EKS module configurations for development environment

--- terraform/environments/development/main.tf
+++ terraform/environments/development/main.tf
@@ -1,10 +1,14 @@
 provider "aws" {
-  region = var.aws_region
+  region = "us-east-1"
 }
 
 module "vpc" {
   source      = "../../modules/vpc"
-  environment = var.environment
-  aws_region  = var.aws_region
+  environment = "dev"
 }
-  
+
+module "eks" {
+  source             = "../../modules/eks"
+  environment        = "dev"
+  private_subnet_ids = module.vpc.private_subnet_ids
+}
