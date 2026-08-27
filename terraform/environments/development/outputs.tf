74b0f5faea3e162020a58778e11c4a26b078a0ad
roberto <robpalacios1@hotmail.com>
2026-08-27 16:14:13 -0500

    feat: add VPC and ECR outputs for the development environment

--- terraform/environments/development/outputs.tf
+++ terraform/environments/development/outputs.tf
@@ -1,3 +1,4 @@
+#VPC Outputs
 output "vpc_id" {
   description = "vpc id"
   value       = module.vpc.vpc_id
@@ -16,4 +17,15 @@ output "private_subnet_ids" {
 output "nat_gateway_id" {
   description = "nat gateway id"
   value       = module.vpc.nat_gateway_id
+}
+
+#ECR Outputs
+output "repository_urls" {
+  description = "the urls of the ECR repositories"
+  value       = module.ecr.repository_urls
+}
+
+output "repository_names" {
+  description = "the names of the ECR repositories"
+  value       = module.ecr.repository_names
 }
