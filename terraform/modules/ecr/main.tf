bd65c922070edb07c410b9945ea3be83ef132ef2
roberto <robpalacios1@hotmail.com>
2026-08-27 16:06:13 -0500

    feat: add terraform module to provision ECR repositories with lifecycle policies

---
+++ terraform/modules/ecr/main.tf
@@ -0,0 +1,53 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+# ====================================================================
+# 1. Create ECR Repository - Users Service - Orders Service - Products Service - 
+# ====================================================================
+
+resource "aws_ecr_repository" "this" {
+
+    for_each = toset(var.repository_names)
+
+    name = "${var.environment}-${each.value}"
+    image_tag_mutability = "IMMUTABLE"
+
+    image_scanning_configuration {
+      scan_on_push = true
+    }
+
+    encryption_configuration {
+      encryption_type = "AES256"
+    }
+
+    tags = {
+        Name = "${var.environment}-${each.value}"
+        environment = "${var.environment}"
+    }
+}
+
+resource "aws_ecr_lifecycle_policy" "this" {
+
+  for_each = toset(var.repository_names)
+  
+  repository = aws_ecr_repository.this[each.key].name
+
+  policy = jsonencode({
+    rules = [
+      {
+        rulePriority = 1
+        description  = "keep only 10 latest images"
+        selection = {
+          tagStatus = "any"
+          tagPrefixList = ["${var.environment}"]
+          countType = "imageCountMoreThan"
+          countNumber = 10
+        }
+        action = {
+          type = "expire"
+        }
+      }
+    ]
+  })
+}
