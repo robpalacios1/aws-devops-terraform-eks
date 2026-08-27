ed1f5b3494b4af7c58e51bbcf8008f76199ad824
roberto <robpalacios1@hotmail.com>
2026-08-27 12:11:04 -0500

    feat: initialize EKS module with infrastructure definitions and environment configuration

---
+++ terraform/modules/eks/variables.tf
@@ -0,0 +1,40 @@
+variable "environment" {
+    description = "The environment (dev, staging, prod)"
+    type        = string
+    default     = "dev"
+}
+
+variable "cluster_version" {
+  description = "EKS cluster version"
+  type        = string
+  default     = "1.36"
+}
+
+variable "private_subnet_ids" {
+  description = "List of private subnet IDs for EKS node group"
+  type        = list(string)
+}
+
+variable "node_desired_size" {
+  description = "Desired number of nodes"
+  type        = number
+  default     = 3
+}
+
+variable "node_max_size" {
+  description = "Maximum number of nodes"
+  type        = number
+  default     = 4
+}
+
+variable "node_min_size" {
+  description = "Minimum number of nodes"
+  type        = number
+  default     = 2
+}
+
+variable "node_instance_type" {
+  description = "EC2 instance type for EKS node group"
+  type        = string
+  default = "t3.medium"
+}
