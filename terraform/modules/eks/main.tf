3748c12239b719f4e26a13620b1aff05f6306923
roberto <robpalacios1@hotmail.com>
2026-09-14 09:35:14 -0500

    Create enable_github_actions and update count condiction

--- terraform/modules/eks/main.tf
+++ terraform/modules/eks/main.tf
@@ -139,14 +139,14 @@ resource "aws_eks_node_group" "main" {
 # ====================================================================
 
 resource "aws_eks_access_entry" "github_actions" {
-  count         = var.github_actions_role_arn != null ? 1 : 0
+  count         = var.enable_github_actions ? 1 : 0
   cluster_name  = aws_eks_cluster.main.name
   principal_arn = var.github_actions_role_arn
   type          = "STANDARD"
 }
 
 resource "aws_eks_access_policy_association" "github_actions" {
-  count         = var.github_actions_role_arn != null ? 1 : 0
+  count         = var.enable_github_actions ? 1 : 0
   cluster_name  = aws_eks_cluster.main.name
   policy_arn    = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
   principal_arn = var.github_actions_role_arn
