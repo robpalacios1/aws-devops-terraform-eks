fb22a9631f4b1224abeae7f11804e54ba29ba479
roberto <robpalacios1@hotmail.com>
2026-09-17 09:42:11 -0500

    update value with ${var.environment}-eks-cluster-role

--- terraform/modules/eks/main.tf
+++ terraform/modules/eks/main.tf
@@ -7,7 +7,7 @@ provider "aws" {
 # ====================================================================
 
 resource "aws_iam_role" "eks_cluster_role" {
-  name = "eks-cluster-role"
+  name = "${var.environment}-eks-cluster-role"
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
