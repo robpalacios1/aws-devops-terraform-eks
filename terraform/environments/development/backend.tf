0b70357926f460085c7ccf6ab33050f2f2c0e67b
roberto <robpalacios1@hotmail.com>
2026-09-17 09:23:03 -0500

    add code for each environment required providers

--- terraform/environments/development/backend.tf
+++ terraform/environments/development/backend.tf
@@ -1,4 +1,17 @@
 terraform {
+  required_version = ">= 1.9.0" 
+    
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = "~> 2.17.0"
+    }
+  }
+  
   backend "s3" {
     bucket         = "my-project-tf-state-us-east-1"
     key            = "development/terraform.tfstate"
